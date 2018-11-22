2b1c769c247166640bb67752414896bfaafedd05
jjslegros <45088345+jjslegros@users.noreply.github.com>
2018-11-22 14:01:21 +0400

    Add files via upload

---
+++ competProjet/src/main/java/com/example/compet/CompetApplication.java
@@ -0,0 +1,12 @@
+package com.example.compet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetApplication.class, args);
+	}
+}
